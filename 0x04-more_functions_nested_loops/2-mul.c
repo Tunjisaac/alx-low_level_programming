0b2650cb6dd9a49b5e2f1a86db1473cdd13cf098
Tester <Olatunjiisaac90@gmail.com>
2022-06-10 09:35:54 +0000

    ; git push pushing empty files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
